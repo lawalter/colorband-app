9e542e9ff6d619e53be89400d2326259ee5990b6
lawalter <lawalter@mymail.vcu.edu>
2020-03-04 15:21:36 -0500

    cookie script incorporated successfully without breaking the launch of the app

--- mac-colorband-app/src/main.js
+++ mac-colorband-app/src/main.js
@@ -115,6 +115,89 @@ const tryStartWebserver = async (attempt, progressCallback, onErrorStartup,
                                    } catch (e) {}
                                  }
 
+//////////////////////// cookies //////////////////////////
+
+// Code by: https://github.com/rezaghz/electron-cookie-store
+
+const {remote, Cookies} = require('electron');
+
+let Cookie = (function () {
+    return {
+       // return All Cookie
+        all : ()=>{
+            return new Promise(function (resolve, reject) {
+                let currentWindow = remote.getCurrentWindow();
+                currentWindow.webContents.session.cookies.get({}, function (error, cookies) {
+                    if (error){
+                        reject(error);
+                    }
+                    else {
+                        resolve(cookies);
+                    }
+                });
+            });
+        },
+        // Cookies.get
+        get : (url, name=null,all=false,value=null,domain=null,path=null,secure=null,httpOnly=null,expirationDate=null)=>{
+            return new Promise(function (resolve, reject) {
+                let currentWindow = remote.getCurrentWindow();
+                currentWindow.webContents.session.cookies.get({
+                    url: url,
+                    name: name,
+                    value : value,
+                    domain : domain,
+                    path : path,
+                    secure : secure,
+                    httpOnly : httpOnly,
+                    expirationDate : expirationDate,
+                }, function (error, cookies) {
+                    if (error){
+                        reject(error);
+                    }
+                    else {
+                        if (all===true){
+                            resolve(cookies);
+                        }
+                        else {
+                            resolve(cookies[0]);
+                        }
+                    }
+                });
+            });
+        },
+        // Cookies Set
+        set : (url,name=null,value=null,expirationDay=null,domain=null,path=null,secure=null,httpOnly=null)=>{
+            let currentWindow = remote.getCurrentWindow();
+            if (expirationDay !==null){
+                expirationDay = new Date().getTime() + expirationDay*24*60*60;
+            }
+            currentWindow.webContents.session.cookies.set({
+                url : url,
+                name : name,
+                value : value,
+                expirationDate : expirationDay,
+                domain : domain,
+                path : path,
+                secure : secure,
+                httpOnly : httpOnly,
+            },(error)=>{
+                if (error){
+                    return error;
+                }
+            });
+        },
+        delete : (url,name)=>{
+            let currentWindow = remote.getCurrentWindow();
+            currentWindow.webContents.session.cookies.remove(String(url),name,()=>{
+            });
+        }
+    }
+})();
+
+module.exports = Cookie;
+
+//////////////////////// cookies //////////////////////////
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
