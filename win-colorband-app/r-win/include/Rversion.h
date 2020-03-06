d6625ec5d1503c351647c8d2051ff44f1f4b7b76
lawalter <lawalter@mymail.vcu.edu>
2020-03-06 16:13:56 -0500

    win build up to add-cran-binary-pkgs.R

---
+++ win-colorband-app/r-win/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 198146
+#define R_NICK "Dark and Stormy Night"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "3"
+#define R_MINOR  "6.2"
+#define R_STATUS ""
+#define R_YEAR   "2019"
+#define R_MONTH  "12"
+#define R_DAY    "12"
+#define R_SVN_REVISION 77560
+#define R_FILEVERSION    3,62,77560,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
